16a4e5f8d3811c4e5686be6e82353cddf07d4272
Tashi <basat1234@gmail.com>
2024-09-12 12:25:35 +0300

    remove comments

--- Terraform-without-modules/eks.tf
+++ Terraform-without-modules/eks.tf
@@ -3,11 +3,8 @@
 
 resource "aws_iam_role" "eks_cluster" {
   # The name of the role
-  name = "itamar-cluster"
+  name = "eks"
 
-  # The policy that grants an entity permission to assume the role.
-  # Used to access AWS resources that you might not normally have access to.
-  # The role that Amazon EKS will use to create AWS resources for Kubernetes clusters
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -28,11 +25,10 @@ POLICY
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
 
 resource "aws_iam_role_policy_attachment" "amazon_eks_cluster_policy" {
-  # The ARN of the policy you want to apply
+
   # https://github.com/SummitRoute/aws_managed_policies/blob/master/policies/AmazonEKSClusterPolicy
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 
-  # The role the policy should be applied to
   role = aws_iam_role.eks_cluster.name
 }
 
@@ -40,22 +36,15 @@ resource "aws_iam_role_policy_attachment" "amazon_eks_cluster_policy" {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_cluster
 
 resource "aws_eks_cluster" "eks" {
-  # Name of the cluster.
-  name = "eks"
 
-  # The Amazon Resource Name (ARN) of the IAM role that provides permissions for 
-  # the Kubernetes control plane to make calls to AWS API operations on your behalf
+  name = "itamar-cluster"
+
+
   role_arn = aws_iam_role.eks_cluster.arn
 
-  # Desired Kubernetes master version
   version = "1.30"
 
   vpc_config {
-    # Indicates whether or not the Amazon EKS private API server endpoint is enabled
-    endpoint_private_access = false
-
-    # Indicates whether or not the Amazon EKS public API server endpoint is enabled
-    endpoint_public_access = true
 
     # Must be in at least two different availability zones
     subnet_ids = [
--- Terraform-without-modules/subnets.tf
+++ Terraform-without-modules/subnets.tf
@@ -2,19 +2,14 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
 
 resource "aws_subnet" "public_1" {
-  # The VPC ID.
   vpc_id = aws_vpc.main.id
 
-  # The CIDR block for the subnet.
   cidr_block = "192.168.0.0/18"
 
-  # The AZ for the subnet.
   availability_zone = "us-east-1a"
 
-  # Required for EKS. Instances launched into the subnet should be assigned a public IP address.
   map_public_ip_on_launch = true
 
-  # A map of tags to assign to the resource.
   tags = {
     Name                        = "public-us-east-1a"
     "kubernetes.io/cluster/eks" = "shared"
@@ -23,19 +18,14 @@ resource "aws_subnet" "public_1" {
 }
 
 resource "aws_subnet" "public_2" {
-  # The VPC ID
   vpc_id = aws_vpc.main.id
 
-  # The CIDR block for the subnet.
   cidr_block = "192.168.64.0/18"
 
-  # The AZ for the subnet.
   availability_zone = "us-east-1b"
 
-  # Required for EKS. Instances launched into the subnet should be assigned a public IP address.
   map_public_ip_on_launch = true
 
-  # A map of tags to assign to the resource.
   tags = {
     Name                        = "public-us-east-1b"
     "kubernetes.io/cluster/eks" = "shared"
@@ -44,16 +34,12 @@ resource "aws_subnet" "public_2" {
 }
 
 resource "aws_subnet" "private_1" {
-  # The VPC ID
   vpc_id = aws_vpc.main.id
 
-  # The CIDR block for the subnet.
   cidr_block = "192.168.128.0/18"
 
-  # The AZ for the subnet.
   availability_zone = "us-east-1a"
 
-  # A map of tags to assign to the resource.
   tags = {
     Name                              = "private-us-east-1a"
     "kubernetes.io/cluster/eks"       = "shared"
@@ -62,13 +48,10 @@ resource "aws_subnet" "private_1" {
 }
 
 resource "aws_subnet" "private_2" {
-  # The VPC ID
   vpc_id = aws_vpc.main.id
 
-  # The CIDR block for the subnet.
   cidr_block = "192.168.192.0/18"
 
-  # The AZ for the subnet.
   availability_zone = "us-east-1b"
 
   # A map of tags to assign to the resource.
